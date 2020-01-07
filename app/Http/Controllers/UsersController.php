e1527861b6b55e9416d9d0fbbd5d89e2acccd29c
郑清华 <450486038@qq.com>
2020-01-07 07:23:09 +0000

    用户注册表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,9 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        dump($request);
+    }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.default')
 @section('title','注册')
 @section('content')
-    <h1>这是一个注册页面</h1>
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>注册</h5>
+            </div>
+            <div class="card-body">
+                <form action="{{route('users.store')}}" method="post">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="name">用户名:</label>
+                        <input type="text" name="name" class="form-control" id="name" value="{{old('name')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">邮箱:</label>
+                        <input type="text" name="email" class="form-control" id="email" value="{{old('email')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码：</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
 @endsection
