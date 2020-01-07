4eda904cce7cb9f51874e251ebf2040ffb28f2a8
郑清华 <450486038@qq.com>
2020-01-07 09:11:29 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,6 +24,12 @@ class UsersController extends Controller
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:3|confirmed',
         ]);
-        return;
+        $user = User:: create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request-> password),
+        ]);
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',$user);
     }
 }
