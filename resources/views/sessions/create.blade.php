347cb2b6671ce0a4bd64e195f9e6f0d3c012773c
郑清华 <450486038@qq.com>
2020-01-09 02:23:27 +0000

    创建会话

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,29 @@
+@extends("layouts.default")
+@section('title','登陆')
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>登陆</h5>
+            </div>
+            <div class="card-body">
+                @include("shared._errors")
+                <form action="{{route('login')}}" method="post">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="email">邮箱:</label>
+                        <input type="text" class="form-control" id="email" name="email" value="{{old('email')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">密码:</label>
+                        <input type="password" class="form-control" id="password" name="password" value="{{old('password')}}">
+                    </div>
+                    <button type="submit" class="btn btn-primary">登陆</button>
+                </form>
+                <hr>
+                <p>还没有账号？<a href="{{route('signup')}}">现在注册</a></p>
+            </div>
+        </div>
+
+    </div>
+@endsection
